4171a324b32e3cfd450d7d9785aad33f6df18e81
Rogoda <rogoda0@gmail.com>
2018-09-21 20:57:29 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern_Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
